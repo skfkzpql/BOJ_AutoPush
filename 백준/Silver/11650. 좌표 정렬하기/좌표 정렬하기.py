cb4c6ba42deae6eef09db745d89c96504ad55c8f
skfkzpql <skfkzpql@gmail.com>
2024-01-31 08:06:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 304 ms, Memory: 54260 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+import sys
+a, *data=map(int,sys.stdin.read().rsplit())
+
+l=[(int(data[i]),int(data[i+1])) for i in range(0,len(data),2) ]
+
+b=sorted(l,key=lambda k: (k[0],k[1]))
+for i in range(len(b)):
+    print(b[i][0],b[i][1])
