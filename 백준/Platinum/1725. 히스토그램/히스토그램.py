fe6d5075df368d5be55377b414e45fc0021d547c
skfkzpql <skfkzpql@gmail.com>
2024-02-21 09:12:26 +0900

    [Platinum V] Title: 히스토그램, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.py
+++ 백준/Platinum/1725. 히스토그램/히스토그램.py
@@ -1,35 +1,21 @@
-def largest_rectangle_area_histogram(heights):
-    n = len(heights)
-    
-    # 스택을 활용한 가장 큰 직사각형의 넓이 계산 함수
-    def calculate_largest_rectangle():
-        stack = []
-        max_area = 0
-        
-        for i in range(n):
-            while stack and heights[i] < heights[stack[-1]]:
-                height = heights[stack.pop()]
-                width = i if not stack else i - stack[-1] - 1
-                max_area = max(max_area, height * width)
-            
-            stack.append(i)
-        
-        while stack:
-            height = heights[stack.pop()]
-            width = n if not stack else n - stack[-1] - 1
-            max_area = max(max_area, height * width)
-        
-        return max_area
-    
-    # 가장 큰 직사각형의 넓이 계산
-    max_area = calculate_largest_rectangle()
-    
-    return max_area
+import sys
 
-# 입력 받기
-N = int(input())
-histogram = [int(input()) for _ in range(N)]
+N, *histogram = map(int,sys.stdin.read().rsplit())
 
-# 결과 출력
-result = largest_rectangle_area_histogram(histogram)
-print(result)
+stack = []
+max_area = 0
+
+for i in range(N):
+    while stack and histogram[i] < histogram[stack[-1]]:
+        height = histogram[stack.pop()]
+        width = i if not stack else i - stack[-1] - 1
+        max_area = max(max_area, height * width)
+
+    stack.append(i)
+
+while stack:
+    height = histogram[stack.pop()]
+    width = N if not stack else N - stack[-1] - 1
+    max_area = max(max_area, height * width)
+
+print(max_area)
