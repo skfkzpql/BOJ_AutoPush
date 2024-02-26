c77f5d0fd3e06c1700bdb027722849ef2c8fac83
skfkzpql <skfkzpql@gmail.com>
2024-02-26 19:00:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        for (int i=1; i <=n; i = i + 2) {
+            answer[(i-1)/2] = i;
+        }
+        return answer;
+    }
+}
